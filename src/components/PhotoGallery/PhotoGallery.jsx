bf05bd244c978d88356fa23e988f226bc8a97824
Carlos Santana <carlossantana331@gmail.com>
2024-11-11 16:46:09 -0600

    responsive

--- src/components/PhotoGallery/PhotoGallery.jsx
+++ src/components/PhotoGallery/PhotoGallery.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import UploadForm from '../UploadForm/UploadForm';
+import React, { useEffect, useState } from "react";
+import UploadForm from "../UploadForm/UploadForm";
 import "./PhotoGallery.css";
 
 function PhotoGallery() {
@@ -15,41 +15,49 @@ function PhotoGallery() {
         `https://www.googleapis.com/drive/v3/files?q='1z_ZdxUzh2llM3ejZeC1wExCfhv6-Vzph'+in+parents&fields=files(id,name,webContentLink,thumbnailLink,mimeType)&key=AIzaSyCdiaGi10jCq4n9L9hKHfYxOljvoD45PCQ&pageSize=100`
       );
       const data = await response.json();
-      console.log(data.files); // Verifica el MIME type aquí
       setMedia(Array.isArray(data.files) ? data.files : []);
     } catch (error) {
       console.error("Error fetching media:", error);
     }
   };
 
+  // Separa las imágenes y los videos
+  const images = media.filter((item) => !item.mimeType.startsWith("video/"));
+  const videos = media.filter((item) => item.mimeType.startsWith("video/"));
+
   return (
     <div className="media-gallery">
       <UploadForm onUpload={fetchMedia} />
       <div className="media-container">
         {media.length > 0 ? (
-          media.map((item) => (
-            <div key={item.id} className="media-item">
-              {item.mimeType.startsWith('video/') ? (
+          <>
+            {/* Renderiza primero las imágenes */}
+            {images.map((item) => (
+              <div key={item.id} className="media-item">
+                <img
+                  src={item.thumbnailLink}
+                  alt={item.name}
+                  className="gallery-image"
+                />
+                <p>{item.name}</p>
+              </div>
+            ))}
+            
+            {/* Renderiza después los videos */}
+            {videos.map((item) => (
+              <div key={item.id} className="media-item">
                 <iframe
                   src={`https://drive.google.com/file/d/${item.id}/preview`}
-                  width="640"
-                  height="480"
                   allow="autoplay; fullscreen"
                   allowFullScreen
                   title={item.name}
                 >
                   Tu navegador no soporta la etiqueta de video.
                 </iframe>
-              ) : (
-                <img
-                  src={item.thumbnailLink} // Usa thumbnailLink para imágenes
-                  alt={item.name}
-                  className="gallery-image"
-                />
-              )}
-              <p>{item.name}</p>
-            </div>
-          ))
+                <p>{item.name}</p>
+              </div>
+            ))}
+          </>
         ) : (
           <p>No se encontraron archivos en la galería.</p>
         )}
